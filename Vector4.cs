505e92bd1478fe7c9895c0f167918530987b7f83
Yve Verstrepen <yve@omnicrash.be>
2017-02-07 17:58:20 +0100

    Update Vector4.cs
    Fixed bug where calling Normalize() would return zero vectors.

--- Vector4.cs
+++ Vector4.cs
@@ -734,7 +734,7 @@ namespace FMath
 
         public static void Normalize(ref Vector4 value, out Vector4 result)
         {
-            result = ZERO;
+            result = value;
             if (value == ZERO) return;
             float lengthInverse = 1.0f / value.Length();
             if (lengthInverse < FMath.EPSILON) return;
